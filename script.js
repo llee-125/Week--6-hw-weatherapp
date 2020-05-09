8626d33418013ccd74d8f2db17705bdb14dae604
llee-125 <llee@berkeley.edu>
2020-05-08 18:51:31 -0700

    API keys and moment working correctly.

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 // set up jQuery
 $(document).ready(function () {
-  var textInput = "";
+  var textInput = "Berkeley";
 
   $("#userSubmit").on("click", function(){
 
   })
 
+  var APIkey = "9f6f9f27da666f2498435db26b1c3410"
+
   $.ajax({
     type: "GET", 
-    url: "https://openweathermap.org/api/one-call&appid=9f6f9f27da666f2498435db26b1c3410",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + textInput + "&appid=" + APIkey,
     dataType: "json",
   }).then(function (response) {
     console.log(response);
+    console.log(response.coord.lon);
+    console.log(response.coord.lat);
 
+    $.ajax({
+      type: "GET",
+      url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + APIkey,
+      dataType: "json",
+    }).then(function(response){
+      console.log(response);
+      var uvi = response.current.uvi;
+    })
 
   // add moment date to header
   var currentDate = moment().format('MMMM DD, YYYY');
-  $("#currentDate").text(currentDate);
+  $(".currentDate").text(currentDate);
 
 // adds moment time to header
   var currentTime = moment().format('h:mm a');
-  $("#currentTime").text(currentTime);
+  $(".currentTime").text(currentTime);
 
 // add textarea on html, line 24
 // textarea should connect to OpenWeather API and search city via API
@@ -32,9 +44,13 @@ $(document).ready(function () {
 // render 5-day forcast tiles in Special Treatment2"
 
 
-});
+  });
+
+    $.ajax({
+      type: "GET",
+      url: "https://api.openweathermap.org/data/2.5/forecast?q=" + textInput + "&appid=" + APIkey,
+    }).then(function(response) {
+      console.log(response);
+    });
 
 });
-
-
-
