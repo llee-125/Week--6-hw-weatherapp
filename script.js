4b5422db1e304f34334d041ff3f0e9939dc8a49f
llee-125 <llee@berkeley.edu>
2020-05-19 15:42:08 -0700

    MANY updates after initial submit of homework.

--- script.js
+++ script.js
@@ -1,57 +1,121 @@
 // set up jQuery
 $(document).ready(function () {
-  var cityName = "San Ramon";
-  console.log(cityName);
+  // var cityName = "San Ramon";
+  // console.log(cityName);
+  var cityHolder = $(".userInput");
+  var tempHolder = $(".temp");
+  var humidityHolder = $(".humidity");
+  var windHolder = $(".wind");
 
-  $("#submit").on("click", function(e){
+  // name variables pulling specific elements from html document
+
+  var cityList = document.querySelector("#city-list");
+
+  // Grab cities from local storage
+  var cities = [];
+
+  var listEl = document.createElement("li");
+  listEl.setAttribute("class", "banana");
+
+  // Clear cityName element and update cityCountSpan
+  function renderCities(citiesArray) {
+    cityList.innerHTML = "";
+
+    // Render a new li for each city
+    for (var i = 0; i < citiesArray.length; i++) {
+      var city = cities[i];
+
+      var li = document.createElement("li");
+      li.textContent = city;
+      li.setAttribute("data-index", i);
+      cityList.appendChild(li);
+    }
+
+    // Set citiesArray into localStorage
+  }
+
+  $("#submit").on("click", function (e) {
     e.preventDefault();
-    var userInput = $("#cityName").val()
-    console.log(userInput)
+    var userInput = $("#cityName").val();
+    // console.log(userInput);
+    cities.push(userInput);
 
-    var APIkey = "9f6f9f27da666f2498435db26b1c3410"
+    var APIkey = "9f6f9f27da666f2498435db26b1c3410";
 
     $.ajax({
-      type: "GET", 
-      url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey,
+      type: "GET",
+      url:
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        userInput +
+        "&appid=" +
+        APIkey +
+        "&units=imperial",
       dataType: "json",
     }).then(function (response) {
-  
-      console.log(response);
-      console.log(response.coord.lon);
-      console.log(response.coord.lat);
+      // console.log(response);
+      // console.log(response.coord.lon);
+      // console.log(response.coord.lat);
 
-      $("#cityInfo").html(response.cityName)
+      $("#cityInfo").html(response.cityName);
+      var wind = response.wind.speed;
+      console.log(wind);
+      var temp = response.main.temp;
+      console.log(temp);
+      var humid = response.main.humidity;
+      console.log(humid);
 
-  
-        $.ajax({
-        type: "GET",
-        url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + APIkey,
-        dataType: "json",
-      }).then(function(response){
-        console.log(response);
-        
-      var uvi = response.current.uvi;
-      })
-  
-    });
-  
+      cityHolder.text(userInput);
+      console.log(userInput);
+      tempHolder.text(temp);
+      console.log(temp);
+      humidityHolder.text(humidity);
+      console.log(humidity);
+      windHolder.text(wind);
+      console.log(wind);
+
       $.ajax({
         type: "GET",
-        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey,
-      }).then(function(response) {
+        url:
+          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+          response.coord.lat +
+          "&lon=" +
+          response.coord.lon +
+          "&appid=" +
+          APIkey,
+        dataType: "json",
+      }).then(function (response) {
         console.log(response);
+
+        var uvi = response.current.uvi;
       });
-  
-  })
+    });
 
-});
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        userInput +
+        "&appid=" +
+        APIkey,
+    }).then(function (response) {
+      console.log(response);
+    });
 
-   // add moment date to header
-  var currentDate = moment().format('MMMM DD, YYYY');
+    renderCities(cities);
+  });
+
+  // function savedCities(){
+  //   var
+  // }
+
+  // add moment date to header
+  var currentDate = moment().format("MMMM DD, YYYY");
   $(".currentDate").text(currentDate);
-  
+
   // adds moment time to header
-  var currentTime = moment().format('h:mm a');
+  var currentTime = moment().format("h:mm a");
   $(".currentTime").text(currentTime);
- 
- 
+
+  // Immediately render cities list on page load/refresh
+  renderCities(cities);
+});
